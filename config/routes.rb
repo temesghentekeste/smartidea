96ca8fcc76815fe513a3b5f25c356c1b50e56308
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-15 17:09:10 +0300

    Add Pages controller & add index, home, profile and explore views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "pages#index"
+
+  get 'home', to: 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'explore', to: "pages#explore"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
