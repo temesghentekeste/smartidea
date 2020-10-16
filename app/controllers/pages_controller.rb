2afd8cc18e988c8678563eba50a2a42a69f088ad
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-16 21:42:52 +0300

    Style the navigation menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,13 +10,13 @@ class PagesController < ApplicationController
     # grap the username from the URL
     if(User.find_by_username(params[:id]))
       @username = params[:id]
+      @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+      @newPost = Post.new
     else
       # redirect to 404 (root for now)
       redirect_to root_path, notice: 'User not found'
     end
 
-    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
-    @newPost = Post.new
   end
   
   def explore
