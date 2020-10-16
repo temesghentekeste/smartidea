73c9f4933a9470a2af803fdc6b75766e95dd29f7
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-16 13:22:24 +0300

    Add registrations controller, add username to signup form, modify routes and pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,13 @@ class PagesController < ApplicationController
   end
 
   def profile
+    # grap the username from the URL
+    if(User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      # redirect to 404 (root for now)
+      redirect_to root_path, notice: 'User not found'
+    end
   end
 
   def explore
